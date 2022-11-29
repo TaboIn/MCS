71d9d11ff7ce0845fae5d2ff906bea033dab34ab
TaboIn <yourlasdream@gmail.com>
2022-11-29 12:18:47 +1000

    change App
    Добавил условия на загрузку

--- practic/shop-react/src/App.js
+++ practic/shop-react/src/App.js
@@ -1,7 +1,30 @@
+import React, { useState } from 'react'
+
 import './App.css'
+import Shop from './components/Shop.js'
 
 function App() {
-	return <div className='App'></div>
+	const [login, setLogin] = useState(false)
+
+	if (login) {
+		return (
+			<>
+				<Shop />
+				<button className='btn' onClick={() => setLogin(false)}>
+					Выйти
+				</button>
+			</>
+		)
+	} else {
+		return (
+			<>
+				<h2>Нужно залогиниться!</h2>
+				<button className='btn' onClick={() => setLogin(true)}>
+					Войти
+				</button>
+			</>
+		)
+	}
 }
 
 export default App
